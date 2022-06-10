fb04b231a0ff5920762d911c600d61f48a4a4fe2
Matt Dean <this.matt.dean@gmail.com>
2022-06-10 10:18:45 -0400

    feat: adds dotnet console application

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace coding_exercise_rts_labs
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
