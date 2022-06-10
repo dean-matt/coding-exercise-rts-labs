0422bf4d959aa14495ef1a02b9976c0b51687837
Matt Dean <this.matt.dean@gmail.com>
2022-06-10 10:22:04 -0400

    feat: declares method for problem 1, above below

--- Program.cs
+++ Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace coding_exercise_rts_labs
 {
@@ -7,6 +8,11 @@ namespace coding_exercise_rts_labs
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+        }
+
+        static Dictionary<string, int> aboveBelow(int[] integers, int comparison)
+        {
+            return new Dictionary<string, int>();
         }
     }
 }
